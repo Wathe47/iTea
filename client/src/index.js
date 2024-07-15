ccf5b6cc960760b5bf32f9d4b6d44e2dbde9528e
Wathe47 <wathsalyagamage23@gmail.com>
2024-07-15 10:10:54 +0530

    set asgardeo configurations

--- client/src/index.js
+++ client/src/index.js
@@ -12,15 +12,15 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
 
-  <AuthProvider
+   <AuthProvider
 
-  config={ {
-   signInRedirectURL: "http://localhost:3000",
-   signOutRedirectURL: "http://localhost:3000",
-   clientID: "Q8zPt1tx2UrNsmSU2YfuLLTVpCga",
-   baseUrl: "https://api.asgardeo.io/t/orgwathsalya",
-   scope: ["openid", "email", "groups", "profile", "roles" ]
-} }
+      config={{
+         signInRedirectURL: "https://9db0e38b-3313-4f87-81b6-335171ff5794.e1-us-east-azure.choreoapps.dev",
+         signOutRedirectURL: "https://9db0e38b-3313-4f87-81b6-335171ff5794.e1-us-east-azure.choreoapps.dev",
+         clientID: "Q8zPt1tx2UrNsmSU2YfuLLTVpCga",
+         baseUrl: "https://api.asgardeo.io/t/orgwathsalya",
+         scope: ["openid", "email", "groups", "profile", "roles"]
+      }}
 
    //  config={ {
    //    signInRedirectURL: "http://localhost:3000",
@@ -39,15 +39,15 @@ root.render(
    //  } }
 
 
-    >
+   >
       <Provider store={store}>
-    <MantineProvider>
+         <MantineProvider>
 
-      <App />
+            <App />
 
-    </MantineProvider>
-  </Provider>
-    </AuthProvider>
+         </MantineProvider>
+      </Provider>
+   </AuthProvider>
 
 
 );
