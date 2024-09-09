afa203c587dc4899e372e8562e9d44cd2875b42a
Wathsalya Gamage <wathsalyagamage23@gmail.com>
2024-09-09 16:10:17 +0530

    updated asgardeo configurations

--- client/src/index.js
+++ client/src/index.js
@@ -14,21 +14,21 @@ root.render(
 
    <AuthProvider
 
-      // config={{
-      //    signInRedirectURL: "https://9db0e38b-3313-4f87-81b6-335171ff5794.e1-us-east-azure.choreoapps.dev",
-      //    signOutRedirectURL: "https://9db0e38b-3313-4f87-81b6-335171ff5794.e1-us-east-azure.choreoapps.dev",
-      //    clientID: "Q8zPt1tx2UrNsmSU2YfuLLTVpCga",
-      //    baseUrl: "https://api.asgardeo.io/t/orgwathsalya",
-      //    scope: ["openid", "email", "groups", "profile", "roles"]
-      // }}
-
       config={{
-         signInRedirectURL: "http://localhost:3000/",
-         signOutRedirectURL: "http://localhost:3000/",
+         signInRedirectURL: "https://itea.onrender.com/",
+         signOutRedirectURL: "https://itea.onrender.com/",
          clientID: "Q8zPt1tx2UrNsmSU2YfuLLTVpCga",
          baseUrl: "https://api.asgardeo.io/t/orgwathsalya",
          scope: ["openid", "email", "groups", "profile", "roles"]
       }}
+
+   // config={{
+   //    signInRedirectURL: "http://localhost:3000/",
+   //    signOutRedirectURL: "http://localhost:3000/",
+   //    clientID: "Q8zPt1tx2UrNsmSU2YfuLLTVpCga",
+   //    baseUrl: "https://api.asgardeo.io/t/orgwathsalya",
+   //    scope: ["openid", "email", "groups", "profile", "roles"]
+   // }}
 
    >
 
